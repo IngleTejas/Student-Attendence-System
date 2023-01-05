b248153dc51e9e9096050eb51cef19dc9ecc70cc
ingletejas <tejasingle1969@gmail.com>
2023-01-05 23:03:58 +0530

    Initial-commit

---
+++ src/data/index.js
@@ -0,0 +1,38 @@
+const employeesData = [
+  
+  {
+    id: 1,
+    firstName: 'Tejas',
+    lastName: 'Ingle',
+    email: '1265',
+    salary: '8:00',
+    date: '5:00'
+  },
+  {
+    id: 2,
+    firstName: 'Taresh',
+    lastName: 'Patil',
+    email: '5345',
+    salary: '10:42',
+    date: '4:42'
+  },
+  {
+    id: 3,
+    firstName: 'Sagar',
+    lastName: 'Ganeshpure',
+    email: '4235',
+    salary: '3:44',
+    date: '8:55'
+  },
+  {
+    id: 4,
+    firstName: 'Uday',
+    lastName: 'Bhalerao',
+    email: '188',
+    salary: '6:15',
+    date: '12:32'
+  },
+  
+];
+
+export { employeesData };
